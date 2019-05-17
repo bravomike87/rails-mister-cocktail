8f642d23df7396e2d486bd8f2e9b02c55244ab79
Bjoern Muellenmeister <b.muellenmeister@gmail.com>
2019-05-17 16:45:03 +0200

    fixed delete function for cocktail

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root to: 'cocktails#index'
-  resources :cocktails, only: [:index, :show, :new, :create] do
+  resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
     resources :doses, only: [:new, :create]
   end
 
   resources :doses, only: [:destroy]
+
 end
